60e862648c6ea8bfafd73245573eaad99cb86c89
Yonis Omar <yomar01@mail.bbk.ac.uk>
2019-11-25 21:18:06 +0000

    initial tests

--- cities.py
+++ cities.py
@@ -24,7 +24,7 @@ def compute_total_distance(road_map):
     the connections in the `road_map`. Remember that it's a cycle, so that 
     (for example) in the initial `road_map`, Wyoming connects to Alabama...
     """
-
+    return 6.55
 
 def swap_cities(road_map, index1, index2):
     """
@@ -37,6 +37,7 @@ def swap_cities(road_map, index1, index2):
     Allow for the possibility that `index1=index2`,
     and handle this case correctly.
     """
+    return ('x','y')
 
 def shift_cities(road_map):
     """
@@ -44,6 +45,10 @@ def shift_cities(road_map):
     to the position i+1. The city at the last position moves to the position
     0. Return the new road map. 
     """
+    road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311), \
+                 ("Delaware", "Dover", 39.161921, -75.526755), \
+                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
+    return road_map1
 
 def find_best_cycle(road_map):
     """
@@ -71,5 +76,3 @@ def main():
 
 if __name__ == "__main__": #keep this in
     main()
-
-print("hello world")
--- test_cities.py
+++ test_cities.py
@@ -7,15 +7,18 @@ def test_compute_total_distance():
                 ("Delaware", "Dover", 39.161921, -75.526755),\
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
     
-    assert compute_total_distance(road_map1)==\
+    assert compute_total_distance(road_map1)== \
            pytest.approx(9.386+18.496+10.646, 0.01)
+    assert compute_total_distance(road_map1) == 4.22
 
     '''add your further tests'''
 
+
 def test_swap_cities():
     '''add your tests'''
+    assert swap_cities(1,2,3) == ('a','b')
 
 def test_shift_cities():
     '''add your tests'''
-
+    assert shift_cities(12) == []
 
